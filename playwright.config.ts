bcba1acd1586ec2514c31e59bc4909a8ce88797b
owner <owner@example.com>
2026-01-21 12:16:00 +0900

    WebKit(Desktop Safari相当)ビジュアルスナップショット追加

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,6 +34,10 @@ export default defineConfig({
       use: { ...devices["Desktop Firefox"] }
     },
     {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] }
+    },
+    {
       name: "mobile-chrome",
       use: { ...devices["Pixel 5"] }
     },
